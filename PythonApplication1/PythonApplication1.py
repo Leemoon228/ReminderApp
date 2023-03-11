f0eb776d320956005452ec6afcd72c7949db54c0
stalk <stalker64024@yandex.ru>
2023-03-11 13:14:04 +0300

    some_packages

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,1 +1,17 @@
-print("Kursach ne sdelan")
+import webbrowser
+
+from win10toast_click import ToastNotifier 
+
+
+
+def open_eyes_healtcare():
+    website = "https://www.wikihow.com/Exercise-Your-Eyes"
+    try:
+        webbrowser.open(website)
+    except:
+        print("Failed to open the download page.")
+              
+toast = ToastNotifier() #так будут реализованы уведомления, нужен UI вместе с окном чтобы настроить инфу для такого
+toast.show_toast("Please make your eyes heathy", "Look into distance, then take a close loot at ur finger, close your eyes fo 20s", duration=30, callback_on_click=open_eyes_healtcare)
+
+print (webbrowser._tryorder)
